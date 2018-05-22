a59b7c682029adc26045ef27e0e235d7c87991cf
Matthew Hielscher <mhielscher@gmail.com>
2018-05-22 10:47:51 -0700

    Switched to commands instead of settings.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # fold-irssi
-fold-irssi is a script for irssi that truncates long messages from specified users (the part "above the fold"), and prints the entire message in a separate window.
+fold-irssi is a script for irssi that truncates long messages from specified users (the part "above the fold"), and prints the original, full message in a separate window.
 
-## Settings
-`/set fold_length <maximum length in characters>`
-`/set fold_users <space-delimited list of nicks>`
+## Usage
+`/fold` displays the list of users with folding enabled.
+`/fold <nick> <limit>` sets a user's fold limit, where `<limit>` is the maximum number of characters to display before the fold.
+`/unfold <nick>` removes a user from the fold list.
+
+
+## Planned Features
+- Fold away entire subsequent messages within a timeout (anti-flood)
+- Access folded messages and threads with a short hash key
+- Log folded messages in special logs, perhaps user.folded.log
+- Provide tool (to begin with) for re-integrating regular logs and fold logs
+- Eventually figure out how to fold the messages on display, but leave logs untouched
--- fold.pl
+++ fold.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 
 # Truncate very long messages from specified users, writing the body of the message to another
-# window, and marking truncated messages with a [Read More] - i.e. putting it "below the fold"
-# for irssi 0.8.19 by Matt Hielscher, based on hilightwin.pl by Timo Sirainen
+# window, and marking truncated messages with a [Read More] - i.e. putting it "below the fold".
+# Written for irssi 0.8.19 by Matt Hielscher.
 use Irssi;
 use vars qw($VERSION %IRSSI);
 
-$VERSION = "dev18.03.11";
+$VERSION = "dev18.05.22";
 %IRSSI = (
     authors	=> "Matt Hielscher",
     contact	=> "matt\@wasabiflux.org", 
@@ -14,49 +14,107 @@ $VERSION = "dev18.03.11";
     description	=> "Truncate and relocate excessively long messages from specified users.",
     license	=> "MIT", # until I choose an actual license and post to GitHub
     url		=> "https://github.com/mhielscher",
-    changed	=> "2018-03-12T11:58-0700"
+    changed	=> "2018-05-22T10:42-07:00"
 );
 
-sub cmd_print_help() {
-  my ($args) = @_;
-  if ($args =~ /^fold(_users)?(_length)? *$/i){
-    my $help = (
-        "/set fold_users <user1> <user2> ...\n".
-        "  Select users to truncate.\n".
-        "/set fold_length <n>\n".
-        "  n = Number of approximate characters to truncate at (word boundaries respected)\n".
-        "\n".
-        "Prints to a window named 'hilight'\n");
-    Irssi::print($help, MSGLEVEL_CLIENTCRAP);
-  }
+my %folded_users = ();
+
+sub cmd_print_help {
+    my ($args) = @_;
+    #if ($args =~ /^fold(_users)?(_length)? *$/i){
+        my $help = (
+            "/fold\n".
+            "  List users and their fold limits.\n".
+            "/fold <nick>\n".
+            "  Show an individual user's fold limit.\n".
+            "/fold <nick> <limit>\n".
+            "  Set a user's fold limit in characters.\n".
+            "/unfold <nick>\n".
+            "  Turn off folding for a user.\n".
+            "\n".
+            "Prints full, original messages to a window named 'hilight'\n"
+        );
+        Irssi::print($help, MSGLEVEL_CLIENTCRAP);
+    #}
+}
+
+# "/fold [nick] [limit]" interacts with the fold list - see comments below
+sub cmd_fold {
+    my ($args) = @_;
+    my ($nick, $limit) = split(' ', $args);
+    # "/fold" without arguments lists the folded users
+    if ($nick eq "") {
+        my $response = "Fold List:\n";
+        foreach my $key (keys %folded_users) {
+            $response = $response."   ".$key.": ".$folded_users{$key}."\n";
+        }
+        $response =~ s/\s+$//; # rtrim
+        Irssi::print($response, MSGLEVEL_CLIENTCRAP);
+    }
+    # "/fold <nick>" returns that user's fold limit
+    elsif ($limit eq "") {
+        if (exists $folded_users{$nick}) {
+            Irssi::print($nick."'s messages are folded at ".$folded_users{$nick}." characters.", MSGLEVEL_CLIENTCRAP);
+        }
+        else {
+            Irssi::print($nick."'s messages are not folded.");
+        }
+    }
+    # "/fold <nick> <limit>" sets a user's fold limit
+    # (if <limit> < 0, unfold that user)
+    else {
+        if ($limit < 0) {
+            cmd_unfold($nick);
+            Irssi::print($nick." removed from folding list.", MSGLEVEL_CLIENTCRAP);
+        }
+        else {
+            $folded_users{$nick} = $limit;
+            Irssi::print($nick."'s messages will now fold at ".$limit." characters.", MSGLEVEL_CLIENTCRAP);
+        }
+    }
+}
+
+# "/unfold <user>" removes that user from the fold list 
+sub cmd_unfold {
+    my ($args) = @_;
+    my ($nick) = split(' ', $args);
+    if ($nick eq "") {
+        Irssi::print("Specify a nick to disable folding.", MSGLEVEL_CLIENTCRAP);
+    }
+    else {
+        delete $folded_users{$nick};
+        Irssi::print($nick." removed from folding list.", MSGLEVEL_CLIENTCRAP);
+    }
 }
 
 sub sig_message_public {
     my ($server, $msg, $nick, $address, $target) = @_;
-    my %users = map {$_ => 1} split(/ /, Irssi::settings_get_str('fold_users'));
-    my $length = Irssi::settings_get_int('fold_length');
 
-    if (exists($users{$nick}) && length($msg) > $length) {
+    if (exists($folded_users{$nick}) && length($msg) > $folded_users{$nick}) {
+        $length = $folded_users{$nick};
         $hilightWindow = Irssi::window_find_name('hilight');
         $fulltext = $target.": <".$nick."> ".$msg;
         $fulltext =~ s/%/%%/g;
         $hilightWindow->print($fulltext, MSGLEVEL_CLIENTCRAP) if ($hilightWindow);
 
         $shortText = substr($msg, 0, rindex($msg, " ", $length));
-        $shortText = substr($msg, 0, $length) if (length($msg) < $length/2);
-        $shortText = $shortText."... [cut]";
+        $shortText = substr($msg, 0, $length) if (length($shortText) < $length/2);
+        $shortText = $shortText."... [cut:".length($shortText)."]";
         Irssi::signal_continue($server, $shortText, $nick, $address, $target);
     }
 }
 
-# default users to fold (none)
-Irssi::settings_add_str('lookandfeel', 'fold_users', '');
-# default length of an "excessively long" message
-Irssi::settings_add_int('lookandfeel', 'fold_length', 140);
 
 $hilightWindow = Irssi::window_find_name('hilight');
-Irssi::print("Creating a window named 'hilight'") if (!$hilightWindow);
-Irssi::Windowitem::window_create("hilight", 2) if (!$hilightWindow);
+if (!$hilightWindow) {
+    Irssi::print("Creating a window named 'hilight'");
+    Irssi::Windowitem::window_create("hilight", 2);
+}
 
 Irssi::signal_add('message public', 'sig_message_public');
-Irssi::command_bind_last('help', 'cmd_print_help');
+Irssi::command_bind('help fold', 'cmd_print_help');
+
+Irssi::command_bind('fold', 'cmd_fold');
+Irssi::command_bind('unfold', 'cmd_unfold');
+
+
